dd364e57273d0e9cc8731be36a51df1c379106ef
KennethOng02 <kennethongkp@gmail.com>
2023-05-13 19:26:12 +0800

    setup echo bot

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const configGet = require('config');
 
+
 // Line config
 const configLine = {
     channelAccessToken:configGet.get('CHANNEL_ACCESS_TOKEN'),
@@ -23,3 +24,26 @@ const port = process.env.PORT || process.env.port || 3000;
 app.listen(port, ()=>{
     console.log(`listening on ${port}`);
 });
+
+app.post('/callback', line.middleware(configLine), (req, res)=>{
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result)=>res.json(result))
+        .catch((err)=>{
+            console.error(err);
+            res.status(500).end();
+    });
+});
+
+function handleEvent(event){
+    if(event.type !== 'message' || event.message.type !== 'text'){
+        return Promise.resolve(null);
+    }
+
+    const echo = {
+        type:'text',
+        text:event.message.text
+    };
+
+    return client.replyMessage(event.replyToken, echo);
+}
