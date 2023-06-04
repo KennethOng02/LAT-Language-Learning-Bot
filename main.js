ddd75117a1fdb854e388b9d0d6bb4f98cb06b3c2
brian098091 <93636904+brian098091@users.noreply.github.com>
2023-06-04 14:53:42 +0800

    final project

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
-"use strict";
+'use strict'
 
-import { imageText, imageTextFile } from "./image-text.js";
-import { textToSpeech } from "./speech.js";
-import { processTranslate } from "./translate.js";
-import { plotData } from "./plot-data.js";
+import { imageText, imageTextFile } from './image-text.js';
+import { textToSpeech } from './speech.js';
+import { processTranslate } from './translate.js';
 
-$(document).ready(function () {
-  $("#analyzeButton").click(function () {
-    imageText();
-  });
+$(document).ready(function(){
+    $("#analyzeButton").click(function(){
+        imageText();
+    });
 
-  $("#inputImageFile").change(function (e) {
-    imageTextFile(e.target.files[0]);
-  });
+    $("#inputImageFile").change(function(e){
+        imageTextFile(e.target.files[0]);
+    });
 
-  $("#translateLanguage").change(async function () {
-    processTranslate($(this).val());
-  });
+    $("#translateLanguage").change(async function() {
+        processTranslate($(this).val());
+    });
 
-  $("#audioPlayButton").click(function () {
-    if ($("#translateLanguage").val() == "zh-Hant")
-      textToSpeech($("#imageText").val(), true);
-    else textToSpeech($("#translateResult").val(), false);
-  });
-
-  $(function () {
-    setInterval(plotData, 5000);
-  });
+    $("#audioPlayButton").click(function() {
+            if ($("#translateLanguage").val() == "zh-Hant")  
+                textToSpeech($("#imageText").val(),true);
+            else 
+                textToSpeech($("#translateResult").val(),false);
+    });
 });
